2c2172176db07166925fa2f7d1846ebae531a94d
mar-hz <103940408+mar-hz@users.noreply.github.com>
2025-03-24 15:53:55 -0600

    changes yippie

--- index.js
+++ index.js
@@ -3,12 +3,16 @@ import express from "express";
 import indexRouter from "./rutas/index_rutas.js ";
 import usersRouter from "./rutas/users_rutas.js";
 import { connectDB } from "./utils/aiven.js";
+import morgan from "morgan";
+import cors from "cors";
 
 const app = express();
 const port = 5000;
 
 const sql = connectDB();
 
+app.use(cors);
+app.use(morgan("dev"));
 app.use(express.json());
 app.use(indexRouter);
 app.use(usersRouter);
